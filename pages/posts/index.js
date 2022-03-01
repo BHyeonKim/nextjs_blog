b266dce3c878dcfa8f27d659d58d5f0e6533788e
Bo Hyeon Kim <rlaqhguse@gmail.com>
2022-03-01 22:34:38 +0900

    all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,42 @@
-function AllPostsPage(props) {}
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excert:
+      'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with server-side rendering.',
+    date: '2022-02-02',
+  },
+  {
+    slug: 'getting-started-with-nextjs2',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excert:
+      'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with server-side rendering.',
+    date: '2022-02-02',
+  },
+  {
+    slug: 'getting-started-with-nextjs3',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excert:
+      'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with server-side rendering.',
+    date: '2022-02-02',
+  },
+  {
+    slug: 'getting-started-with-nextjs4',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excert:
+      'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with server-side rendering.',
+    date: '2022-02-02',
+  },
+];
+
+function AllPostsPage(props) {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
