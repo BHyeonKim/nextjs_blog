7e435a67b80261abd3e905ccf3cf2a3ba37a7c12
Bo Hyeon Kim <rlaqhguse@gmail.com>
2022-03-01 23:44:04 +0900

    Add Functions To Read & Fetch Data From Markdown Files

--- pages/index.js
+++ pages/index.js
@@ -12,30 +12,6 @@ const DUMMY_POSTS = [
       'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with server-side rendering.',
     date: '2022-02-02',
   },
-  {
-    slug: 'getting-started-with-nextjs2',
-    title: 'Getting started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excert:
-      'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with server-side rendering.',
-    date: '2022-02-02',
-  },
-  {
-    slug: 'getting-started-with-nextjs3',
-    title: 'Getting started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excert:
-      'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with server-side rendering.',
-    date: '2022-02-02',
-  },
-  {
-    slug: 'getting-started-with-nextjs4',
-    title: 'Getting started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excert:
-      'NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with server-side rendering.',
-    date: '2022-02-02',
-  },
 ];
 
 function HomePage(props) {
