17a4a26360b92353cd7cbb320d9a69e6ae01bbdf
Bo Hyeon Kim <rlaqhguse@gmail.com>
2022-03-02 23:24:07 +0900

    Rendering Code Snippets From Markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,6 @@
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import Image from 'next/image';
 
 import PostHeader from './post-header';
@@ -27,6 +29,16 @@ function PostContent(props) {
         );
       }
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1];
+      return (
+        <SyntaxHighlighter style={atomDark} language={language}>
+          {children}
+        </SyntaxHighlighter>
+      );
     },
   };
 
