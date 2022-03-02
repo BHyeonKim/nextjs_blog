7a2ab8de53c73d04d8f15451ca1817542a9f453e
Bo Hyeon Kim <rlaqhguse@gmail.com>
2022-03-03 08:49:44 +0900

    The Contact Form

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,7 @@
-function ContactPage(props) {}
+import ContactForm from '../components/contact/contact-form';
+
+function ContactPage(props) {
+  return <ContactForm />;
+}
 
 export default ContactPage;
